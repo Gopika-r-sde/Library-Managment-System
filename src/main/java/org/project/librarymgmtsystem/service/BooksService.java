e648160ad7c0436c071e31a7150c7f5b7bb247de
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-02 14:50:34 +0530

    feat : Added update and delete functions

--- src/main/java/org/project/librarymgmtsystem/service/BooksService.java
+++ src/main/java/org/project/librarymgmtsystem/service/BooksService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,7 +48,7 @@ public class BooksService {
     public ResponseEntity<ResponseStructure<Optional<Books>>> getBookById(Long id) {
         ResponseStructure<Optional<Books>> structure = new ResponseStructure<>();
         Optional<Books> recBook = booksDao.getBookById(id);
-        if (!recBook.isEmpty()) {
+        if (recBook.isPresent()) {
             structure.setMessage("Books found by id Successfully");
             structure.setData(recBook);
             structure.setStatusCode(HttpStatus.NOT_FOUND.value());
@@ -55,6 +56,48 @@ public class BooksService {
         }
         // if no books found with this id throw custom exception
         throw new BooksNotFoundException("No book found with this id :" + id);
+    }
 
+    // Update Book by id
+    public ResponseEntity<ResponseStructure<Books>> updateBookById(Long id, Books books) {
+        ResponseStructure<Books> structure = new ResponseStructure<>();
+
+        Optional<Books> recBooks = booksDao.getBookById(id);
+
+        if (!recBooks.isPresent()) {
+            Books existBook = recBooks.get();
+            existBook.setTitle(books.getTitle());
+            existBook.setAuthor(books.getAuthor());
+            existBook.setPublisher(books.getPublisher());
+            existBook.setCategory(books.getCategory());
+            existBook.setIsbn(books.getIsbn());
+            existBook.setPrice(books.getPrice());
+            existBook.setTotalCopies(books.getTotalCopies());
+            existBook.setAvailableCopies(books.getAvailableCopies());
+
+            Books updatedBook = booksDao.addBooks(existBook);
+
+            structure.setMessage("Books Updated Successfully");
+            structure.setData(updatedBook);
+            structure.setStatusCode(HttpStatus.OK.value());
+
+            return new ResponseEntity<ResponseStructure<Books>>(structure, HttpStatus.OK);
+        }
+        throw new BooksNotFoundException("No book found with this id :" + id);
+    }
+
+    // Delete Book by id
+    public ResponseEntity<ResponseStructure<String>> deleteBookById(Long id) {
+        ResponseStructure<String> structure = new ResponseStructure<>();
+        Optional<Books> recBook = booksDao.getBookById(id);
+        if (recBook.isPresent()) {
+            booksDao.deleteBookById(id);
+            structure.setMessage("Books Deleted Successfully");
+            structure.setData("Deleted book with id" + id);
+            structure.setStatusCode(HttpStatus.OK.value());
+
+            return new ResponseEntity<ResponseStructure<String>>(structure, HttpStatus.NO_CONTENT);
+        }
+        throw new  BooksNotFoundException("No book found with this id :" + id);
     }
 }
