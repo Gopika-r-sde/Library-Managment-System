6f3f4c0b2270c18ed797e2e556c2feda941be766
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-02 13:31:04 +0530

    feat : Created two tables in database

--- src/main/java/org/project/librarymgmtsystem/entity/Books.java
+++ src/main/java/org/project/librarymgmtsystem/entity/Books.java
@@ -1,12 +1,33 @@
 package org.project.librarymgmtsystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Books {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
+    @Column(nullable = false)
+    private String publisher;
+    @Column(nullable = false)
+    private String category;
+    @Column(nullable = false)
+    private String isbn;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false)
+    private int totalCopies;
+    @Column(nullable = false)
+    private int availableCopies;
 }
